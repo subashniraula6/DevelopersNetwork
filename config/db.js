527d4c44b842041d89233c96e620d80b39c97926
Basanta Niraula <subashniraula6@gmail.com>
2025-02-23 16:33:23 -0500

    removed config library replaces with dotenv

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const config = require('../config.js')
-console.log("Config........", config.get('mongoURI'))
 const db = config.get('mongoURI')
 const connectDB = async () => {
     try {
