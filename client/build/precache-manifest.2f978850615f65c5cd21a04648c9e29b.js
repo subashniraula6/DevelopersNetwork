22c294e47e33ecb9dc4e89265016e3d70d877416
Basanta Niraula <subashniraula6@gmail.com>
2025-02-22 21:21:39 -0500

    added dockerfile for cicd pipeline

---
+++ client/build/precache-manifest.2f978850615f65c5cd21a04648c9e29b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d4ee953e6e212a323b9bd2431219b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "737cd5a08fc57e67c36b",
+    "url": "/static/css/main.0320cd3a.chunk.css"
+  },
+  {
+    "revision": "15a469fae80a827feaa1",
+    "url": "/static/js/2.c267ebee.chunk.js"
+  },
+  {
+    "revision": "307ab7e595dce2c0d585cefbaa8b4d68",
+    "url": "/static/js/2.c267ebee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737cd5a08fc57e67c36b",
+    "url": "/static/js/main.8d7531a0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c5c5d50fddb6a70bb07da4b77d32e530",
+    "url": "/static/media/loading.c5c5d50f.gif"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
