86b98a1d88fb954f9651effd7098e22f18bafbc5
Basanta Niraula <subashniraula6@gmail.com>
2025-03-01 22:48:08 -0500

    change environment variable format

--- config.js
+++ config.js
@@ -1,36 +1,23 @@
-const fs = require('fs');
-const path = require('path');
+const process = require('process');
 
 let config = {};
 
 function loadConfigFromEnv() {
-  const configJson = process.env.MY_CONFIG_JSON;
-  if (configJson) {
-    try {
-      config = JSON.parse(configJson);
-      console.log('Configuration loaded from GitHub Secrets.');
-    } catch (error) {
-      console.error('Error parsing JSON configuration from secrets:', error);
-    }
+  const mongoURI = process.env.MONGO_URI;
+  const jwtSecret = process.env.JWT_SECRET;
+  const githubClientId = process.env.GITHUB_CLIENT_ID;
+  const githubSecret = process.env.GITHUB_SECRET;
+
+  if (mongoURI && jwtSecret && githubClientId && githubSecret) {
+    config = {
+      mongoURI,
+      jwtSecret,
+      githubClientId,
+      githubSecret,
+    };
+    console.log('Configuration loaded from environment variables.');
   } else {
-    console.log('GitHub Secrets not found. Loading from production.json.');
-    loadConfigFromFile();
-  }
-}
-
-function loadConfigFromFile() {
-  const configFilePath = path.join(__dirname, 'config', 'production.json'); // Assumes config directory is in the same directory as config.js
-
-  try {
-    const fileData = fs.readFileSync(configFilePath, 'utf8');
-    config = JSON.parse(fileData);
-    console.log('Configuration loaded from production.json.');
-  } catch (error) {
-    console.error('Error loading configuration from production.json:', error);
-    if(Object.keys(config).length === 0){
-        console.error("Application cannot start without configuration")
-        process.exit(1);
-    }
+    console.log('No backend configuration found in environment variables.');
   }
 }
 
