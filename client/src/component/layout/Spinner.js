f44f8840317fe585b14c91c9a48ae23c2ca58d81
Basanta Niraula <subashniraula6@gmail.com>
2025-03-08 15:33:39 -0500

    updated css loader

--- client/src/component/layout/Spinner.js
+++ client/src/component/layout/Spinner.js
@@ -4,7 +4,7 @@ import spinner from '../../img/loading.gif'
 const Spinner = () => {
     return (
         <>
-           <img src={spinner} style={{width: '70px', marginLeft: '35vw', marginTop: '30vh'}} alt="Loading...."/> 
+           <img src={spinner} style={{width: '70px', marginLeft: '30vw', marginTop: '30vh'}} alt="Loading...."/> 
         </>
     )
 }
