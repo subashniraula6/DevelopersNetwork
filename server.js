049f878c269c7b8e3bcd799cbdb36d5f404cf826
Basanta Niraula <subashniraula6@gmail.com>
2025-08-24 16:27:18 -0400

    Changed Backend Port to 6000

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 6000
 const app = express()
 const path = require('path');
 require('dotenv').config();
